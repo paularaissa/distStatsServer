c00b77c947074fb316cc70d163eccd6c23414c8a
Paula Silva <paula.r.silva@inesctec.pt>
2019-06-18 15:42:16 +0100

    anova

--- R/getAnova.R
+++ R/getAnova.R
@@ -20,60 +20,64 @@
 #' @export
 
 getAnova <- function(beta, formula, media.y) {
+  
+  
+  return("OK")
+  
   #Data transformations
-  beta.reg.aux <- as.numeric(unlist(strsplit(beta, split="x")))
-  beta.reg <- data.matrix(beta.reg.aux)
-  mean.y <- as.numeric(media.y)
-
-  bindxy <- getVarbyFormula(formula=formula)
-  bind.x <- data.matrix(bindxy$x)
-  bind.y <- data.matrix(bindxy$y)
-
-  y.hat <- fittedDS(beta=beta.reg, x=bind.x)
-
-  #prediction error
-  residuals <- bind.y - y.hat
-
-  #lenght bind_x
-  lbx <- length(bind.x)
-
-  #rows x
-  rows.x <- nrow(bind.x)
-  #rows y
-  rows.y <- nrow(bind.y)
-
-  #cols x
-  cols.x <- ncol(bind.x)
-  #cols y
-  cols.y <- ncol(bind.y)
-
-  #k number of estimatiors
-  k <- cols.x - 1
-
-  s2 <- sum(residuals ^ 2) / (rows.x - k - 1)
-  s <- sqrt(s2)
-
-  #sse : sum of squares error
-  sse <- sum(residuals ^ 2)
-
-  #sst : total sum of squares
-  sst <- sum((bind.y - mean.y) ^ 2)
-
-  #standard error of the estimate
-  std.residuals <- sum(bind.y - residuals)
-
-  #sum of total squares
-  quad.y <- sum(bind.y ^ 2)
-
-  ## estimate of sigma-squared
-  ##dSigmaSq <- sum((bind.y - bind.x%*%beta.reg)^2)/(nrow(bind.x)-ncol(bind.x))
-  ##d.sigma.sq.part <- sum((bind.y - bind.x%*%beta.reg)^2)
-
-  #Standardized
-  residuos.std <- residuals / s
-
-
-  return(list(rows.x = rows.x, rows.y = rows.y, cols.x = cols.x, cols.y=cols.y, residuals = residuals, sst = sst, sse = sse,
-             lbx = lbx, quad.y = quad.y, y.hat = y.hat, std.residuals = std.residuals))
+  # beta.reg.aux <- as.numeric(unlist(strsplit(beta, split="x")))
+  # beta.reg <- data.matrix(beta.reg.aux)
+  # mean.y <- as.numeric(media.y)
+  # 
+  # bindxy <- getVarbyFormula(formula=formula)
+  # bind.x <- data.matrix(bindxy$x)
+  # bind.y <- data.matrix(bindxy$y)
+  # 
+  # y.hat <- fittedDS(beta=beta.reg, x=bind.x)
+  # 
+  # #prediction error
+  # residuals <- bind.y - y.hat
+  # 
+  # #lenght bind_x
+  # lbx <- length(bind.x)
+  # 
+  # #rows x
+  # rows.x <- nrow(bind.x)
+  # #rows y
+  # rows.y <- nrow(bind.y)
+  # 
+  # #cols x
+  # cols.x <- ncol(bind.x)
+  # #cols y
+  # cols.y <- ncol(bind.y)
+  # 
+  # #k number of estimatiors
+  # k <- cols.x - 1
+  # 
+  # s2 <- sum(residuals ^ 2) / (rows.x - k - 1)
+  # s <- sqrt(s2)
+  # 
+  # #sse : sum of squares error
+  # sse <- sum(residuals ^ 2)
+  # 
+  # #sst : total sum of squares
+  # sst <- sum((bind.y - mean.y) ^ 2)
+  # 
+  # #standard error of the estimate
+  # std.residuals <- sum(bind.y - residuals)
+  # 
+  # #sum of total squares
+  # quad.y <- sum(bind.y ^ 2)
+  # 
+  # ## estimate of sigma-squared
+  # ##dSigmaSq <- sum((bind.y - bind.x%*%beta.reg)^2)/(nrow(bind.x)-ncol(bind.x))
+  # ##d.sigma.sq.part <- sum((bind.y - bind.x%*%beta.reg)^2)
+  # 
+  # #Standardized
+  # residuos.std <- residuals / s
+  # 
+  # 
+  # return(list(rows.x = rows.x, rows.y = rows.y, cols.x = cols.x, cols.y=cols.y, residuals = residuals, sst = sst, sse = sse,
+  #            lbx = lbx, quad.y = quad.y, y.hat = y.hat, std.residuals = std.residuals))
 
 }
